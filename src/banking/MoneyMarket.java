b18b5ae2ade27c6fd63e5bbff9f33f9e90bc816b
ktalla <70788915+ktalla@users.noreply.github.com>
2023-10-14 15:06:07 -0400

    Update MoneyMarket.java

--- src/banking/MoneyMarket.java
+++ src/banking/MoneyMarket.java
@@ -1,4 +1,54 @@
 package banking;
 
-public class MoneyMarket {
+public class MoneyMarket extends Savings{
+
+    private int withdrawal; //number of withdrawls
+    private static final double INTEREST_RATE = 4.5;
+
+    public MoneyMarket(){
+
+    }
+
+    public MoneyMarket(Profile holder, double balance, int withdrawal) {
+        super(holder, balance, true);
+        this.withdrawal = withdrawal;
+    }
+
+    public int getWithdrawal() {
+        return withdrawal;
+    }
+
+    public void setWithdrawal(int withdrawal) {
+        this.withdrawal = withdrawal;
+    }
+
+    @Override
+    public double monthlyInterest() {
+        if(isLoyal){
+            double interestRate = INTEREST_RATE+0.25;
+            return (balance*(interestRate/100))/12;
+        }
+        else{
+            return (balance*(INTEREST_RATE/100))/12;
+        }
+    }
+
+    @Override
+    public double monthlyFee() {
+        if(balance>=2000){
+            return 0;
+        }
+        else{
+            return Savings.MONTHLY_FEE;
+        }
+    }
+    @Override
+    public int compareTo(Account account) {
+        if(account instanceof MoneyMarket){
+            return super.compareTo(account);
+        }
+        else {
+            return account.getClass().getName().compareTo(this.getClass().getName());
+        }
+    }
 }
