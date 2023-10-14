3ff58bd378ae2041ccf0f446c64aef15bbb030e1
ktalla <70788915+ktalla@users.noreply.github.com>
2023-10-14 15:04:57 -0400

    Update Account.java

--- src/banking/Account.java
+++ src/banking/Account.java
@@ -1,4 +1,23 @@
 package banking;
 
-public class Account {
+public abstract class Account implements Comparable<Account> {
+    protected Profile holder;
+    protected double balance;
+
+    public Account(){
+
+    }
+
+    public Account(Profile holder, double balance){
+        this.holder = holder;
+        this.balance = balance;
+    }
+    public abstract double monthlyInterest();
+    public abstract double monthlyFee();
+
+    @Override
+    public int compareTo(Account account) { //comparing accounts by their holders
+        return account.holder.compareTo(this.holder);
+    }
 }
+
