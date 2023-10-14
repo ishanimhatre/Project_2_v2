d717b3ffc0ee0f616f7656932db3c475a81098a7
ktalla <70788915+ktalla@users.noreply.github.com>
2023-10-14 15:06:47 -0400

    Update CollegeChecking.java

--- src/banking/CollegeChecking.java
+++ src/banking/CollegeChecking.java
@@ -1,4 +1,26 @@
 package banking;
 
-public class CollegeChecking {
+public class CollegeChecking extends Checking{
+
+    private Campus campus; //campus code
+    public CollegeChecking(Profile holder, double balance, Campus campus){
+        super(holder, balance);
+        this.campus = campus;
+    }
+
+    @Override
+    public double monthlyFee() {
+        return 0.0;
+    }
+
+    @Override
+    public int compareTo(Account account) {
+        if(account instanceof CollegeChecking){
+            return super.compareTo(account);
+        }
+        else {
+            return account.getClass().getName().compareTo(this.getClass().getName());
+        }
+    }
 }
+
